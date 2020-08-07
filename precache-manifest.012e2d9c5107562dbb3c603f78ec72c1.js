e4c60c7890a331d6fba30e1ff8c7dd8800a3ebe9
gaurav-kalita <gauravkalita.nlp@gmail.com>
2020-08-07 23:18:22 +0530

    Updates

---
+++ precache-manifest.012e2d9c5107562dbb3c603f78ec72c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ba3eb2c7d944215ac85cd3efd5b43b",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "61988edb8dbb5a066767",
+    "url": "/tictactoe/static/css/main.ed6338e4.chunk.css"
+  },
+  {
+    "revision": "7b18134325272a9912ca",
+    "url": "/tictactoe/static/js/2.b2ab5219.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tictactoe/static/js/2.b2ab5219.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61988edb8dbb5a066767",
+    "url": "/tictactoe/static/js/main.5d74cb82.chunk.js"
+  },
+  {
+    "revision": "8fd1009f88de2a8f7d1e",
+    "url": "/tictactoe/static/js/runtime-main.57baa19c.js"
+  }
+]);
